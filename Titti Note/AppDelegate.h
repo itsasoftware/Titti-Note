906b0c80b03b3e7b8a23ea31273854c076b8d612
Andrea Asioli <ilmionuovoaccount@gmail.com>
2017-08-01 16:16:54 +0200

    Initial Commit

---
+++ Titti Note/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Titti Note
+//
+//  Created by Popolo on 01/08/17.
+//  Copyright © 2017 itsasoftware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
